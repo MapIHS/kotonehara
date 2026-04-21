5d68b6f2c859ea549656ce3f3be71995820a9bc1
MapIhs <maptuhul2011@gmail.com>
2026-04-21 15:18:28 +0700

    Sticker lottie

--- pkg/getpp.go
+++ pkg/getpp.go
@@ -2,6 +2,7 @@ package pkg
 
 import (
 	"context"
+	"strings"
 
 	"github.com/MapIHS/kotonehara/internal/clients"
 	"github.com/MapIHS/kotonehara/internal/commands"
@@ -18,33 +19,44 @@ func init() {
 		As:       []string{"getpp", "getprofilepicture"},
 		IsPrefix: true,
 		Exec: func(ctx context.Context, client *clients.Client, m *message.Message, cfg config.Config) {
+			var targetJID types.JID
+
 			if m.QuotedMsg != nil {
-				jidStr := ""
-				if m.QuotedMsg.GetParticipant() != "" {
-					jidStr = m.QuotedMsg.GetParticipant()
-				} else if len(m.QuotedMsg.MentionedJID) > 0 {
-					jidStr = m.QuotedMsg.MentionedJID[0]
+				if ext := m.Message.GetExtendedTextMessage(); ext != nil && ext.GetContextInfo() != nil {
+					participant := ext.GetContextInfo().GetParticipant()
+					if participant != "" {
+						targetJID, _ = types.ParseJID(participant)
+					}
 				}
+			} else if m.ID != nil && len(m.ID.MentionedJID) > 0 {
+				targetJID, _ = types.ParseJID(m.ID.MentionedJID[0])
+			} else if m.Query != "" {
+				user := strings.TrimSpace(m.Query)
+				user = strings.ReplaceAll(user, "@", "")
+				user = strings.ReplaceAll(user, " ", "")
+				user = strings.ReplaceAll(user, "-", "")
+				targetJID = types.NewJID(user, "s.whatsapp.net")
+			}
 
-				jid, err := types.ParseJID(jidStr)
-				if err != nil {
-					m.Reply(ctx, "Failed to parse JID from quoted message")
-					return
-				}
-				pp, err := client.WA.GetProfilePictureInfo(ctx, jid, &whatsmeow.GetProfilePictureParams{})
-				if err != nil {
-					m.Reply(ctx, err.Error())
-					return
-				}
+			if targetJID.IsEmpty() {
+				m.Reply(ctx, "Balas pesan orangnya, tag, atau masukkan nomornya untuk mendapatkan profile picture.")
+				return
+			}
 
-				buff, err := client.FetchBytes(pp.URL)
-				if err != nil {
-					m.Reply(ctx, err.Error())
-					return
-				}
-				_, err = client.SendImage(ctx, m.From, buff, "", m.ID)
-			} else {
-				m.Reply(ctx, "Kosong keknya")
+			pp, err := client.WA.GetProfilePictureInfo(ctx, targetJID, &whatsmeow.GetProfilePictureParams{})
+			if err != nil {
+				m.Reply(ctx, "Gagal mendapatkan profile picture (mungkin diprivasi): "+err.Error())
+				return
+			}
+
+			buff, err := client.FetchBytes(pp.URL)
+			if err != nil {
+				m.Reply(ctx, "Gagal mengunduh gambar: "+err.Error())
+				return
+			}
+			_, err = client.SendImage(ctx, m.From, buff, "", m.ID)
+			if err != nil {
+				m.Reply(ctx, "Gagal mengirim gambar: "+err.Error())
 			}
 		},
 	})
