9339e2c36c2e681211ee74667dfe6a97253e0ed6
MapIHS <maptuhul2011@gmail.com>
2026-05-30 01:56:58 +0700

    fix x

--- internal/clients/send.go
+++ internal/clients/send.go
@@ -2,8 +2,10 @@ package clients
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"go.mau.fi/whatsmeow"
@@ -53,15 +55,48 @@ func (c *Client) FetchBytes(u string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0 (compatible; kotonehara/1.0; +https://github.com/MapIHS/kotonehara)")
+	req.Header.Set("Accept", "*/*")
+
 	resp, err := c.HTTP.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	return io.ReadAll(resp.Body)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		snippet := strings.TrimSpace(string(body))
+		if len(snippet) > 120 {
+			snippet = snippet[:120]
+		}
+		if snippet == "" {
+			return nil, fmt.Errorf("http %d saat download media", resp.StatusCode)
+		}
+		return nil, fmt.Errorf("http %d saat download media: %s", resp.StatusCode, snippet)
+	}
+
+	if len(body) == 0 {
+		return nil, fmt.Errorf("media kosong dari %s", u)
+	}
+
+	return body, nil
 }
 
 func (c *Client) SendVideo(ctx context.Context, to types.JID, data []byte, gifPlayback bool, caption string, opts *waE2E.ContextInfo) (whatsmeow.SendResponse, error) {
+	mime := http.DetectContentType(data)
+	if mime == "application/octet-stream" {
+		mime = "video/mp4"
+	}
+	if !strings.HasPrefix(mime, "video/") {
+		return whatsmeow.SendResponse{}, fmt.Errorf("data bukan video valid (mime: %s)", mime)
+	}
+
 	up, err := c.WA.Upload(ctx, data, whatsmeow.MediaVideo)
 	if err != nil {
 		return whatsmeow.SendResponse{}, err
@@ -74,10 +109,10 @@ func (c *Client) SendVideo(ctx context.Context, to types.JID, data []byte, gifPl
 			MediaKey:      up.MediaKey,
 			Caption:       proto.String(caption),
 			GifPlayback:   proto.Bool(gifPlayback),
-			Mimetype:      proto.String(http.DetectContentType(data)),
+			Mimetype:      proto.String(mime),
 			FileEncSHA256: up.FileEncSHA256,
 			FileSHA256:    up.FileSHA256,
-			FileLength:    proto.Uint64(uint64(len(data))),
+			FileLength:    &up.FileLength,
 			ContextInfo:   opts,
 		},
 	}
